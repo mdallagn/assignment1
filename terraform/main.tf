7bca8f071f5d7b1ea0797485b2afb66a4eb192d8
mdallagn <madall-agnese@myseneca.ca>
2023-07-27 10:41:50 +0000

    changes to terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,9 @@
+# Define the AWS provider and specify the region as us-east-1
 provider "aws" {
   region = "us-east-1"
 }
 
+# Fetch the latest Amazon Linux AMI filtered by its name
 data "aws_ami" "websrv_amazon_linux" {
   owners      = ["amazon"]
   most_recent = true
@@ -11,14 +13,17 @@ data "aws_ami" "websrv_amazon_linux" {
   }
 }
 
+# Fetch available AWS availability zones for the region
 data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# Fetch the default AWS VPC (Virtual Private Cloud)
 data "aws_vpc" "default" {
   default = true
 }
 
+# Create an AWS EC2 instance resource
 resource "aws_instance" "web_server" {
   ami                  = data.aws_ami.websrv_amazon_linux.id
   key_name             = aws_key_pair.assignment1.key_name
@@ -26,21 +31,27 @@ resource "aws_instance" "web_server" {
   instance_type        = var.instance_type
   iam_instance_profile = "LabInstanceProfile"
   security_groups      = [aws_security_group.web_sg.id]
+
+  # Define tags for the instance
   tags = merge({
     Name = "${var.prefix}-WebServer"
     }
   )
 }
 
+# Create an AWS key pair resource used for SSH access
 resource "aws_key_pair" "assignment1" {
   key_name   = var.prefix
   public_key = file("assignment1.pub")
 }
 
+# Create a public subnet in the default VPC with a non-conflicting CIDR block
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = var.public_subnet_cidrs
+  cidr_block              = "172.31.128.0/20"  # Choose a non-overlapping CIDR block for the new subnet
   map_public_ip_on_launch = true
+
+  # Define tags for the subnet
   tags = merge(
     var.default_tags, {
       Name = "${var.prefix}-Public-Subnet"
@@ -48,11 +59,13 @@ resource "aws_subnet" "public_subnet" {
   )
 }
 
+# Create a security group for the web server instances
 resource "aws_security_group" "web_sg" {
-  name        = "websrv-SG"
+  name        = "webserver-SG"
   description = "SG for web server"
   vpc_id      = data.aws_vpc.default.id
 
+  # Allow incoming HTTP traffic from everywhere
   ingress {
     description = "HTTP from everywhere"
     from_port   = 80
@@ -61,6 +74,7 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow incoming SSH traffic from everywhere
   ingress {
     description = "SSH from everywhere"
     from_port   = 22
@@ -69,6 +83,7 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow incoming traffic on port 8081 from everywhere
   ingress {
     description = "HTTP from everywhere"
     from_port   = 8081
@@ -77,6 +92,7 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow incoming traffic on port 8082 from everywhere
   ingress {
     description = "HTTP from everywhere"
     from_port   = 8082
@@ -85,6 +101,7 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow incoming traffic on port 8083 from everywhere
   ingress {
     description = "HTTP from everywhere"
     from_port   = 8083
@@ -93,12 +110,15 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow all outgoing traffic to everywhere
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # Define tags for the security group
   tags = merge(
     var.default_tags, {
       Name = "${var.prefix}-WebSrv-SG"
@@ -106,10 +126,14 @@ resource "aws_security_group" "web_sg" {
   )
 }
 
-resource "aws_ecr_repository" "websrv_images" {
-  name = "websrv-images"
+/*
+# Create an AWS Elastic Container Registry (ECR) repository for web server images
+resource "aws_ecr_repository" "webserver_images" {
+  name = "webserver_images-images"
 }
 
+# Create an AWS Elastic Container Registry (ECR) repository for MySQL images
 resource "aws_ecr_repository" "mysql_images" {
   name = "mysql-images"
 }
+*/
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,26 +1,34 @@
+# Define a variable "default_tags" with default values for common tags used across resources.
+# This variable is of type "map(any)" and serves as a collection of key-value pairs.
 variable "default_tags" {
   default = {
     "Owner" = "marco"
     "App"   = "Web"
   }
   type        = map(any)
-  description = "Default tag"
+  description = "Default tag: Common tags applied to various resources."
 }
 
+# Define a variable "prefix" that sets a default value "Assignment1".
+# This variable is of type "string" and represents a group used as a prefix for resource names.
 variable "prefix" {
   default     = "Assignment1"
   type        = string
-  description = "Group used as prefix"
+  description = "Group used as prefix: A string used as a prefix for resource names."
 }
 
+# Define a variable "instance_type" that sets a default value "t3.micro".
+# This variable is of type "string" and specifies the type of EC2 instance to be launched.
 variable "instance_type" {
   default     = "t3.micro"
   type        = string
-  description = "Type of the instance"
+  description = "Type of the instance: The EC2 instance type for the web server."
 }
 
+# Define a variable "public_subnet_cidrs" that sets a default value "172.31.96.0/20".
+# This variable is of type "string" and represents the CIDR block for the public subnet.
 variable "public_subnet_cidrs" {
   default     = "172.31.96.0/20"
   type        = string
-  description = "Public subnet CIDR block"
+  description = "Public subnet CIDR block: The CIDR block for the public subnet."
 }
